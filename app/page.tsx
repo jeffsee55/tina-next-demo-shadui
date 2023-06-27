a94daeeb6fc03ca6122cf1761e22ec168cdbfa7c
Jeff See <jeffsee.55@gmail.com>
2023-06-27 11:25:42 -0700

    Add revalidate for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { client } from "@/tina/__generated__/client"
 
 import { ClientPage } from "@/components/app/page"
 
+export const revalidate = 60 // revalidate this page every 60 seconds
+
 export default async function Page() {
   const result = await client.queries.pageAndNav({ relativePath: "home.md" })
 
